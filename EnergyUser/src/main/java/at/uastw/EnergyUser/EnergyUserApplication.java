7da2c96d3bc6b8b19a1e4adaf42de2f36bb578e2
manuel <wi24b001@technikum-wien.at>
2026-03-30 17:41:01 +0200

    test

--- EnergyUser/src/main/java/at/uastw/EnergyUser/EnergyUserApplication.java
+++ EnergyUser/src/main/java/at/uastw/EnergyUser/EnergyUserApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EnergyUserApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(EnergyUserApplication.class, args);
 	}
