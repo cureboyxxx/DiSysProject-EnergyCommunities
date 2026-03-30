da942f408ae0b827a034527398661d5d24de5a0f
manuel <wi24b001@technikum-wien.at>
2026-03-30 17:39:20 +0200

    init

---
+++ EnergyUser/src/main/java/at/uastw/EnergyUser/EnergyUserApplication.java
@@ -0,0 +1,13 @@
+package at.uastw.EnergyUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyUserApplication.class, args);
+	}
+
+}
---
+++ UsageService/src/test/java/at/uastw/UsageService/UsageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.uastw.UsageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
