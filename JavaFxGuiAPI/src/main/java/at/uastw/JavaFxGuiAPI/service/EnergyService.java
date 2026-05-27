bbb47d53658905ac514ea4946de6f6ac1a6e8987
Elias Grünbacher <118984127+cureboyxxx@users.noreply.github.com>
2026-05-27 19:16:02 +0200

    add line to have more commits hehe

--- JavaFxGuiAPI/src/main/java/at/uastw/JavaFxGuiAPI/service/EnergyService.java
+++ JavaFxGuiAPI/src/main/java/at/uastw/JavaFxGuiAPI/service/EnergyService.java
@@ -17,6 +17,7 @@ public class EnergyService {
         );
     }
 
+
     public List<HistoricalEnergyResponse> getHistoricalEnergy(String start, String end) {
         // Mock data for 2026-04-01
         // Mock energy community with about 10 single family homes equipped with solar panels
